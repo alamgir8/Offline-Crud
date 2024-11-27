3469f8c65bca8d7133db27009698136eae2e8393
alamgir8 <ahossain38900@gmail.com>
2024-11-27 17:17:41 +0600

    feat(v-3): updated index db intilization

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,32 @@
 import { openDB } from "idb";
 import { DB_NAME, STORE_NAME } from "./constants.js";
 
-export const db = await openDB(DB_NAME, 1, {
-  upgrade(db) {
-    const store = db.createObjectStore(STORE_NAME, {
-      keyPath: "id",
-      autoIncrement: true,
-    });
-    store.createIndex("status", "status");
-    store.createIndex("createdAt", "createdAt");
-  },
-});
+// export const db = await openDB(DB_NAME, 1, {
+//   upgrade(db) {
+//     const store = db.createObjectStore(STORE_NAME, {
+//       keyPath: "id",
+//       autoIncrement: true,
+//     });
+//     store.createIndex("status", "status");
+//     store.createIndex("createdAt", "createdAt");
+//   },
+// });
+let db;
+async function initDB() {
+  db = await openDB(DB_NAME, 1, {
+    upgrade(db) {
+      const store = db.createObjectStore(STORE_NAME, {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      store.createIndex("status", "status");
+      store.createIndex("createdAt", "createdAt");
+    },
+  });
+}
+
+initDB(); // Initialize the database during runtime
+export { db };
 
 export const dbOperations = {
   async addTask(task) {
